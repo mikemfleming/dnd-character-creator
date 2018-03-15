d62bc7aa1980e36ca3aeac00b7443bb9e70a8510
Mike Fleming <flemingatx@gmail.com>
2018-03-15 16:29:51 -0500

    redux hello world

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HelloWorld from './containers/HelloWorld';
+
+class App extends Component {
+	render() {
+		return (
+			<HelloWorld />
+		);
+	}
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert('hell yeah!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import helloReducer from './reducers';
+import App from './App';
+
+let store = createStore(helloReducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
